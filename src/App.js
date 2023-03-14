07a0f9c6bef9e97a98bce80b246619254717a3c0
Senthamizhselvan6065 <118972019+Senthamizhselvan6065@users.noreply.github.com>
2023-03-14 16:33:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,49 +12,41 @@ function App() {
       productImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz142xu2lOjBs5MMTghUSvEuD9DhoDrTlYrA&usqp=CAU",
       productName:"iPhone 14 Pro Max",
       price:"Rs 1,40,000",
-      rating:"",
     },
     {
       productImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSORqsMx8qD74trBePpv5c2Gu54RxRiSCxuAQ&usqp=CAU",
       productName:"Google Pixeal 7 Pro",
       price:"Rs 40,000",
-      rating:"",
     },
     {
       productImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUF3MkkF4SnVBX2U0kaEGRSrMJRQYU4DlOQg&usqp=CAU",
       productName:"Oneplus 11 Pro",
       price:"Rs 50,000",
-      rating:"",
     },
     {
       productImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgVgLZ9mFMEFNPFUZzhMuXyr4Y9f0c4QCA1Q&usqp=CAU",
       productName:"IQOO Neo 7",
       price:"Rs 30,000",
-      rating:"",
     },
     {
       productImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSh5RyeaWBsLuediz6pkA093wsdh1Ast53bVg&usqp=CAU",
       productName:"Redmi Note 10 Pro/Max",
       price:"Rs 18,000",
-      rating:"",
     },
     {
       productImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsVIToKVT1D8gRIFtGi5qtPIJiQDvWBJ1YuA&usqp=CAU",
       productName:"Oppo Reno 8 Pro",
       price:"Rs 40,000",
-      rating:"",
     },
     {
       productImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA5DOiLj87vwtZ_wvVfTZipHQMqHsTrZbagA&usqp=CAU",
       productName:"Oppo Find N2",
       price:"Rs 80.000",
-      rating:"",
     },
     {
       productImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxnS_Njl44S2bMpT-TCen3QLqK4a0_9ycF-mC-1UvHCXXq8bQXOAaFbvOwM3M8m8Kwqj8&usqp=CAU",
       productName:"Realme 10 Pro Plus",
       price:"Rs 22,000",
-      rating:"",
     }
   ]
   const [count, setCount] = useState(0)
@@ -94,10 +86,7 @@ function Cards({prod, idx, setCount, count}){
     <Card.Body>
       <Card.Title>{prod.productName}</Card.Title>
       <Card.Text>{prod.price}</Card.Text>
-      <Card.Text>{prod.rating}</Card.Text>
-
      {!show ?  <Button onClick={addToCardt} className='btn-1' variant="primary">Add to Card</Button> : ""}
-
       {show ? <Button onClick={removeToCardt} className='btn-2' variant="danger">Remove Now</Button> : ""}
     </Card.Body>
   </Card>
